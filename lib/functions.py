37ba9a5f92ea83621f426a712c8b5b7fee8755b5
catherine <wachukimani@gmail.com>
2023-05-15 22:10:39 +0300

    testing error

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    return "Hello, programmer!"
+
+print(greet_programmer())
+
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+greet("Naureen")
+
+def greet_with_default(name="programmer!"):
+    print(f"Hello, {name}")
+
+greet_with_default()
+greet_with_default("Sunny!")
+
+
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(1,2))
+
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return "null"
+
+    return number / 2
+
+print(halve(4))
+print(halve("two")) 
