0d76160c39ab57827bbcbe5602185d8c69ff7bfe
catherine <wachukimani@gmail.com>
2023-05-16 15:23:34 +0300

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,37 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    return "Hello, programmer!"
-
-print(greet_programmer())
-
-
+    print("Hello, programmer!")
 
 def greet(name):
     print(f"Hello, {name}!")
 
-greet("Naureen")
 
-def greet_with_default(name="programmer!"):
-    print(f"Hello, {name}")
-
-greet_with_default()
-greet_with_default("Sunny!")
-
-
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+greet_with_default("Guido")
 
 
 def add(num1, num2):
-    return num1 + num2
-print(add(1,2))
+    return(num1 + num2)
+
 
 
 
 def halve(number):
-    if not isinstance(number, (int, float)):
+    if type(number)!= int and type(number)!=float:
         return "null"
 
-    return number / 2
+    return(number/2)
 
-print(halve(4))
-print(halve("two")) 
+
